263a3e62a32f3b6cae9f51c1699855f7eb6c28a0
Molly Brown <mollbrown@gmail.com>
2017-04-04 12:45:11 -0600

    change login password input to password input type.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-  # attr_accessible :email, :password, :password_confirmation
   attr_accessor :password
 
   validates :email, :password, presence: true
@@ -8,12 +7,4 @@ class User < ApplicationRecord
 
   has_secure_password
 
-
-
 end
-
-
-# I cannot sign up with an email address that has already been used.
-# I cannot sign up without an email address and a password.
-# Password and confirmation must match.
-# If criteria is not met the user should be given a message to reflect the reason they could not sign up.
