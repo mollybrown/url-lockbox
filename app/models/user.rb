af55ac379217f011f8130ecd9c2cc4977fa4249e
Molly Brown <mollbrown@gmail.com>
2017-04-04 12:12:19 -0600

    add user model validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  validates :email, presence: true
+  validates :email, uniqueness: true
 
   has_secure_password
-  
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    context "invalid attributes" do
+
+      it "is invalid without an email" do
+        user = User.new(password: "password")
+        expect(user).to be_invalid
+      end
+
+      it "is invalid without a password" do
+        user = User.new(email: "molly@molly.com")
+        expect(user).to be_invalid
+      end
+
+    end
+  end
 end
