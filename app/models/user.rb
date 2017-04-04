c031f97552e7f4cc6dda8d73e81fd6eb203b1250
Molly Brown <mollbrown@gmail.com>
2017-04-04 12:43:34 -0600

    add input validation flash messages to login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ApplicationRecord
-  validates :email, presence: true
+  # attr_accessible :email, :password, :password_confirmation
+  attr_accessor :password
+
+  validates :email, :password, presence: true
   validates :email, uniqueness: true
+  validates_confirmation_of :password
 
   has_secure_password
 
+
+
 end
+
+
+# I cannot sign up with an email address that has already been used.
+# I cannot sign up without an email address and a password.
+# Password and confirmation must match.
+# If criteria is not met the user should be given a message to reflect the reason they could not sign up.
