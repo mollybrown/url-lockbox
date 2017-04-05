c20c9709edbeb0638a1201df1d1e3148787d4736
Molly Brown <mollbrown@gmail.com>
2017-04-04 18:33:48 -0600

    Add filter.js and filter field on index

---
+++ app/assets/javascripts/filter.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $("#filter").keyup(function(){
+  });
+
+  $("#unread-links").on("click", function(event){
+  });
+
+  $("#read-links").on("click", function(event){
+  });
+
+})
